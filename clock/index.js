b61af54360ad9da204b499768e96f8d406548360
Jaijithks <kathirjas20@gmail.com>
2025-12-13 17:06:35 +0530

    Add files via upload

---
+++ clock/index.js
@@ -0,0 +1,11 @@
+function updateclock(){
+    const now = new Date()
+    const hours = now.getHours().toString().padStart(2, 0);
+    const min = now.getMinutes().toString().padStart(2, 0);
+    const sec = now.getSeconds().toString().padStart(2, 0);
+    const timestring =  `${hours}:${min}:${sec}`;
+    document.getElementById("clockcontent").textContent = timestring;
+}
+
+updateclock();
+setInterval(updateclock, 1000);
