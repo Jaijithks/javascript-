88f141f0e07c5da5cd5defe04891eee059465e68
Jaijithks <kathirjas20@gmail.com>
2025-12-13 19:49:08 +0530

    Update index.js

--- clock/index.js
+++ clock/index.js
@@ -1,9 +1,12 @@
 function updateclock(){
     const now = new Date()
-    const hours = now.getHours().toString().padStart(2, 0);
+    let hours = now.getHours();
+    const merdian = hours > 12 ? "AM" : "PM";
+    hours = hours % 12 || 12;
+    hours= hours.toString().padStart(2, 0);
     const min = now.getMinutes().toString().padStart(2, 0);
     const sec = now.getSeconds().toString().padStart(2, 0);
-    const timestring =  `${hours}:${min}:${sec}`;
+    const timestring =  `${hours}:${min}:${sec} ${merdian}`;
     document.getElementById("clockcontent").textContent = timestring;
 }
 
