9d46d595c5bf62a85533fdede30b7f31176ee531
Jaijithks <kathirjas20@gmail.com>
2026-01-27 14:13:27 +0530

    Delete eventlistener(mouse)forfun directory

--- eventlistener(mouse)forfun/index.js
+++
@@ -1,9 +0,0 @@
-const boxx = document.getElementById("mybox");
-   let colorss = ["red","blue","green","yellow","tomato","orange","white","black"];
-function change(event){
-    let indi = Math.floor(Math.random()*8);
-    document.body.style.backgroundColor = colorss[indi];
-    
-};
-
-boxx.addEventListener("click",change);
